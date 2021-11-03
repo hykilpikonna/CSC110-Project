c248e500a203abe7a6070cb5da5604689bdf201c
Hykilpikonna <me@hydev.org>
2021-11-03 15:34:32 -0400

    [+] Create load config function

---
+++ data-collection/twitter_individual.py
@@ -0,0 +1,18 @@
+import os
+
+import json5
+import tweepy
+
+
+def load_config() -> dict:
+    """
+    Load config using JSON5, from either the local file ~/config.json5 or from the environment variable named config.
+
+    :return: Config dictionary object
+    """
+    if os.path.isfile('config.json5'):
+        with open('config.json5', 'r') as f:
+            return json5.load(f)
+    else:
+        return json5.loads(os.getenv('config'))
+
