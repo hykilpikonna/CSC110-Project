1af5182f91619663a1ad4d94481c3f6ae6a886ea
Hykilpikonna <me@hydev.org>
2021-11-03 16:00:05 -0400

    [+] Get tweets from inidivdual

--- collect/twitter_individual.py
+++ collect/twitter_individual.py
@@ -2,11 +2,17 @@ import os
 
 import json5
 import tweepy
+from tweepy.models import Status
 
 from collect.utils import *
 
 
 if __name__ == '__main__':
     conf = load_config()
+    api = tweepy_login(conf)
 
+    tweets: list[Status] = api.user_timeline(screen_name='voxdotcom', count=200, tweet_mode = 'extended')
+
+    for tweet in tweets:
+        print(tweet._json)
 
