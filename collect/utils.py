00be8d7d40de9c9d11e1112b71f74276ab16e129
Hykilpikonna <me@hydev.org>
2021-11-03 15:59:44 -0400

    [U] Update usage in tweepy login

--- collect/utils.py
+++ collect/utils.py
@@ -51,7 +51,7 @@ def tweepy_login(conf: Config) -> tweepy.API:
     :param conf: Config from load_config()
     :return: Tweepy API object
     """
-    auth = tweepy.OAuthHandler(conf['consumer_key'], conf['consumer_secret'])
-    auth.set_access_token(conf['access_token'], conf['access_secret'])
+    auth = tweepy.OAuthHandler(conf.consumer_key, conf.consumer_secret)
+    auth.set_access_token(conf.access_token, conf.access_secret)
     api: tweepy.API = tweepy.API(auth)
     return api
