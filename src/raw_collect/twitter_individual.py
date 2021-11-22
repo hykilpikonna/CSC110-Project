b55b4f3e4cebb0774db2a5c00182558c1a2af727
Hykilpikonna <me@hydev.org>
2021-11-22 00:24:56 -0500

    Revert "[-] Remove testing script"
    This reverts commit 963256ce167c2e0f089b52bb8af06d8363ce403d.

---
+++ src/raw_collect/twitter_individual.py
@@ -0,0 +1,10 @@
+from collect.twitter import tweepy_login, download_user_tweets
+from collect.utils import *
+
+
+if __name__ == '__main__':
+    conf = load_config()
+    api = tweepy_login(conf)
+
+    download_user_tweets(api, 'sauricat')
+
