b84576275499466aa56df4a9329337c5bbe7ed22
Hykilpikonna <me@hydev.org>
2021-11-24 19:34:50 -0500

    [+] Parse markdown

--- src/report.py
+++ src/report.py
@@ -1,14 +1,16 @@
+import markdown
+
 from constants import SRC_DIR
 from utils import read
 
 
 def generate_report() -> str:
     """
-    Create a HTML report
+    Do data visualization and generate a HTML report
 
     :return: HTML
     """
-    return read(f'{SRC_DIR}/report_document.md')
+    return markdown.markdown(read(f'{SRC_DIR}/report_document.md'))
 
 
 if __name__ == '__main__':
